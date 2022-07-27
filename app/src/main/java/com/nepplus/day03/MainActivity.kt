98f307dcf855ce44d17e5bd8d0290be713a014d0
이규행 <2kyuhang@naver.com>
2022-07-27 11:30:12 +0900

    한칸에 보여질 모양을 별도의 Layout로 생성

--- app/src/main/java/com/nepplus/day03/MainActivity.kt
+++ app/src/main/java/com/nepplus/day03/MainActivity.kt
@@ -2,10 +2,34 @@ package com.nepplus.day03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nepplus.day03.adapters.StudentListViewAdapter
+import com.nepplus.day03.datas.StudentData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<StudentData>()
+    lateinit var mStudentListViewAdapter: StudentListViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //학생 더미 데이터
+        mStudentList.add( StudentData("김민철", "서울시 서대문구", 1990) )
+        mStudentList.add( StudentData("김민철", "서울시 서대문구", 1994) )
+        mStudentList.add( StudentData("함상민", "서울시 종로구", 1993) )
+        mStudentList.add( StudentData("허신지", "서울시 서대문구", 1999) )
+        mStudentList.add( StudentData("유수아", "서울시 마포구", 2002) )
+        mStudentList.add( StudentData("윤예은", "서울시 마포구", 2002) )
+        mStudentList.add( StudentData("윤예은", "서울시 마포구", 2002) )
+        mStudentList.add( StudentData("윤예은", "서울시 마포구", 2002) )
+        mStudentList.add( StudentData("윤예은", "서울시 마포구", 2002) )
+        mStudentList.add( StudentData("윤예은", "서울시 마포구", 2002) )
+
+        //어답터 객체화 => 우리가 만들어놓은 ArrayList를 전달하는 코드
+        mStudentListViewAdapter = StudentListViewAdapter(this, R.layout.student_list_item, mStudentList)
+
+        mainListView.adapter = mStudentListViewAdapter
     }
 }
--- app/src/main/java/com/nepplus/day03/adapters/StudentListViewAdapter.kt
+++ app/src/main/java/com/nepplus/day03/adapters/StudentListViewAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import com.nepplus.day03.R
 import com.nepplus.day03.datas.StudentData
+import kotlinx.android.synthetic.main.student_list_item.view.*
 
 class StudentListViewAdapter(
     val mContext : Context,
@@ -21,9 +22,15 @@ class StudentListViewAdapter(
             tempRow = LayoutInflater.from(mContext).inflate(R.layout.student_list_item, null)//inflate = 연결해주는 것
         }
         val row = tempRow!!
+
         //2 실제 데이터 반영
+        row.nameTxt.text = mList[position].name
+        row.addressTxt.text = mList[position].address
+        //row.ageTxt.text = mList[position].birthYear.toString()
 
-
+        //변수를 활용한 String 가공
+        val koreanAge = 2022 - mList[position].birthYear + 1
+        row.ageTxt.text = "(${koreanAge}세)"
         return row
     }
 
